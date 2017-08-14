16bab2c2d80494451713c72030132a907b3d1dd8
z1g1 <mail@zackglick.com>
2017-08-14 16:47:18 -0700

    up to test_search_bookstore_by_book_title in bookstoretests working

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,39 @@
 class Bookstore(object):
-    pass
+	def __init__ (self, name): 
+		self.name = name 
+		self.books = []
+	def get_books(self):
+		return self.books
 
+	def add_book(self, book):
+		self.books.append(book)
 
+	def search_books(self, title=None, author=None):
+		answer = []		
+		# need to figure out if we are searching by title or author
+		if (title == None) and author:
+			# search by Author
+			pass
+			print("searching by author")
+			print(author.name)
+			for book in self.books:
+				print(book.author.name)
+		elif title and (author == None):
+			# Search by title 
+			for book in self.books:
+				if title.lower() in book.title.lower():
+					answer.append(book)
+		return answer
 class Author(object):
-    pass
+	def __init__ (self, name, nationality): 
+		self.name = name
+		self.nationality = nationality
 
+	def get_books(self):
+		authors_books = [self.name]
 
+		return authors_books
 class Book(object):
-    pass
+	def __init__ (self, title, author):
+		self.title = title
+		self.author = author
